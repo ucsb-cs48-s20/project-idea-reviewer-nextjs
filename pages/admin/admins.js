ac5377a38501e846ef8705328d613be8e2fa02fc
Bryan Terce <bryan.terce@gmail.com>
2020-04-10 20:57:47 -0700

    bt - add logic for adding admins

--- pages/admin/admins.js
+++ pages/admin/admins.js
@@ -1,8 +1,10 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import Head from "next/head";
 import useSWR from "swr";
 import fetch from "isomorphic-unfetch";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
 import BootstrapTable from "react-bootstrap-table-next";
 import { getAdmins } from "../api/admins";
 import Layout from "../../components/Layout";
@@ -41,6 +43,21 @@ export default function ManageAdminsPage(props) {
 
   const { data, mutate } = useSWR("/api/admins", { initialData });
 
+  const [newAdminEmail, setNewAdminEmail] = useState("");
+
+  const addAdmin = useCallback(async () => {
+    setNewAdminEmail("");
+    await mutate([...data, { email: newAdminEmail }], false);
+    await fetch("/api/admins", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: newAdminEmail }),
+    });
+    await mutate();
+  }, [newAdminEmail]);
+
   const deleteAdmin = useCallback(async (adminId) => {
     await mutate(
       data.filter((u) => u._id !== adminId),
@@ -68,6 +85,17 @@ export default function ManageAdminsPage(props) {
         <title>Manage Admins</title>
       </Head>
       <h1>Manage Admins</h1>
+      <Form className="mb-5">
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <FormControl
+            type="text"
+            value={newAdminEmail}
+            onChange={(e) => setNewAdminEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Button onClick={addAdmin}>Add Admin</Button>
+      </Form>
       <BootstrapTable keyField="_id" data={data} columns={columns} />
     </Layout>
   );
