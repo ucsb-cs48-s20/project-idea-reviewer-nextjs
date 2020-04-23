9fe8e2a7e52056ebe9ba1873a6b58f658a895283
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-04-23 12:59:13 -0700

    pc - change == to === for comparing strings in JavaScript

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 function mongodb_uri() {
-  if (process.env.NODE_ENV == "production") {
+  if (process.env.NODE_ENV === "production") {
     return process.env.MONGODB_URI_PRODUCTION;
-  } else if (process.env.NODE_ENV == "staging") {
+  } else if (process.env.NODE_ENV === "staging") {
     return process.env.MONGODB_URI_STAGING;
   }
   return process.env.MONGODB_URI;
