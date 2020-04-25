18bdbec35ae3c777bc4786cecb89d8e5f661a9c6
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-04-25 16:53:49 -0700

    pc - refactor utils/email and tests

--- __tests__/email.js
+++ __tests__/email.js
@@ -1,13 +1,23 @@
 import { reformatEmail } from "../utils/email";
 
-describe("emails", () => {
-  it("converts @umail.ucsb.edu to @ucsb.edu", () => {
-    expect(reformatEmail("tkomarlu@umail.ucsb.edu")).toBe("tkomarlu@ucsb.edu");
-  });
+describe("utils/email", () => {
+  describe("reformatEmail", () => {
+    it("converts @umail.ucsb.edu to @ucsb.edu", () => {
+      expect(reformatEmail("tkomarlu@umail.ucsb.edu")).toBe(
+        "tkomarlu@ucsb.edu"
+      );
+    });
 
-  it("throws when receiving invalid inputs", () => {
-    expect(() => {
-      reformatEmail("");
-    }).toThrow();
+    it("throws when receiving invalid inputs", () => {
+      expect(() => {
+        reformatEmail(42);
+      }).toThrow("email should be of type string");
+    });
+
+    it("throws when receiving invalid inputs", () => {
+      expect(() => {
+        reformatEmail("");
+      }).toThrow("email should not be an empty string");
+    });
   });
 });
