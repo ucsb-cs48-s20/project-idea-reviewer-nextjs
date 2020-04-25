b98cf64c550a227ca0eaa0a9e6c1c12ebaec9463
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-04-25 14:46:17 -0700

    pc - factor out loading page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,12 +1,15 @@
 describe("Home Page", () => {
-  it("has a nav bar", () => {
+  beforeEach(() => {
+    // runs before each test in the block
     cy.visit("http://localhost:3000");
+  });
 
+  it("has a nav bar", () => {
     // a nav element with class navbar
     cy.get("nav.navbar").should("exist");
   });
+
   it("has a home page brand button on nav bar", () => {
-    cy.visit("http://localhost:3000");
     cy.get("nav.navbar * a.navbar-brand").should("exist");
   });
 });
