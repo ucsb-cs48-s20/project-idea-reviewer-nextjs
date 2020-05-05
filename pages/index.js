369faeb0a94460e976767f237d0cf6d7c161d1a3
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-05-05 01:27:07 -0700

    cb - wip index.js loading user ideas

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,42 @@ import { optionalAuth } from "../utils/ssr";
 import { Alert } from "react-bootstrap";
 import Head from "next/head";
 import { useCallback, useState } from "react";
+import fetch from "isomorphic-unfetch";
+import useSWR from "swr";
 
 export const getServerSideProps = optionalAuth;
 
-function getPageContents(user) {
-  if (!user) {
-    return <Alert variant="default">Please log in to use this app</Alert>;
-  } else if (!user.role) {
-    // guest user
-    return <Alert variant="danger">You are not enrolled in this course!</Alert>;
-  } else if (user.role === "admin") {
-    return (
-      <Alert variant="warning">Admins can not submit project ideas!</Alert>
-    );
-  } else {
-    return <IdeaForm />;
-  }
-}
-
 export default function HomePage(props) {
   const user = props.user;
-  getPageContents(user);
+
+  const { ideas } = useSWR("/api/ideas", fetch, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+
+  if (!ideas) {
+    return <h1>Loading</h1>;
+  }
+
   return (
     <Layout user={user}>
       <Head>
         <title>Project Idea Reviewer</title>
       </Head>
       <h1>Welcome to the Project Idea Reviewer!</h1>
-      {getPageContents(user)}
+      {!user && (
+        <Alert variant="default">Please log in to use this application</Alert>
+      )}
+      {user.role == "guest" && (
+        <Alert variant="danger">You are not enrolled in this course</Alert>
+      )}
+      {user.role == "admin" && (
+        <Alert variant="warning">Admins can not submit project ideas</Alert>
+      )}
+      {user.role == "student" && ideas != null && ideas == 0 && <IdeaForm />}
+      {user.role == "student" && ideas != null && ideas == 1 && (
+        <p>you have an idea (describe it here)</p>
+      )}
     </Layout>
   );
 }
