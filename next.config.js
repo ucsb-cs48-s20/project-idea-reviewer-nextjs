3eea8e42497b333e36fed07cb942ede67b743dee
Bryan Terce <bryan.terce@gmail.com>
2020-05-07 13:42:20 -0700

    bt/tk - setup cypress to run app in test auth mode

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ function mongodb_uri() {
 
 module.exports = {
   env: {
+    USE_TEST_AUTH: process.env.USE_TEST_AUTH,
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
