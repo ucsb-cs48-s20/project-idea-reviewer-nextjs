9f35acfa02d6c81b603cc0dc4d86438e0613699a
andrewhlu <alu@ucsb.edu>
2020-05-08 14:58:53 -0700

    AL/VG - Add template for delete review endpoint

---
+++ pages/api/reviews/[ideaId]/[authorId].js
@@ -0,0 +1,38 @@
+import { ObjectId } from "mongodb";
+import { authenticatedAction } from "../../../../utils/api";
+import { initDatabase } from "../../../../utils/mongodb";
+
+async function deleteIdea(ideaId) {
+  const client = await initDatabase();
+  const ideas = client.collection("ideas");
+
+  const query = {
+    _id: ObjectId(ideaId),
+  };
+
+  const result = await ideas.deleteOne(query);
+
+  if (!result.deletedCount) {
+    throw {
+      status: 404,
+      message: "Idea not found",
+    };
+  }
+}
+
+async function performAction(req, user) {
+  const { ideaId, authorId } = req.query;
+
+  if (user.role !== "admin") {
+    throw { status: 403 };
+  }
+
+  switch (req.method) {
+    case "DELETE":
+      return deleteIdea(ideaId);
+  }
+
+  throw { status: 405 };
+}
+
+export default authenticatedAction(performAction);
