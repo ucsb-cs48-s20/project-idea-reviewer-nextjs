9b9d89b88fa61f00fefb4b80136dfb8b25704884
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-05-17 19:30:06 -0700

    pc/sc - remove offending nested <p> element in AppFooter

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -8,21 +8,18 @@ function AppFooter() {
       <Card style={{ maxWidth: "60rem" }}>
         <Card.Body>
           <Card.Text>
-            <p>
-              This project from{" "}
-              <a href="http://ucsb-cs48.github.io">CMPSC 48</a> at{" "}
-              <a href="http://www.ucsb.edu">UC&nbsp;Santa&nbsp;Barbara</a>{" "}
-              adapts{" "}
-              <a href="http://www.liberatingstructures.com/12-2510-crowd-sourcing/">
-                25/10 Crowdsourcing
-              </a>{" "}
-              for remote instruction, to generate project ideas for a software
-              engineering course. Check out the source code on{" "}
-              <a href="https://github.com/ucsb-cs48-s20/project-idea-reviewer-nextjs">
-                GitHub
-              </a>
-              .
-            </p>
+            This project from <a href="http://ucsb-cs48.github.io">CMPSC 48</a>{" "}
+            at <a href="http://www.ucsb.edu">UC&nbsp;Santa&nbsp;Barbara</a>{" "}
+            adapts{" "}
+            <a href="http://www.liberatingstructures.com/12-2510-crowd-sourcing/">
+              25/10 Crowdsourcing
+            </a>{" "}
+            for remote instruction, to generate project ideas for a software
+            engineering course. Check out the source code on{" "}
+            <a href="https://github.com/ucsb-cs48-s20/project-idea-reviewer-nextjs">
+              GitHub
+            </a>
+            .
           </Card.Text>
         </Card.Body>
       </Card>
